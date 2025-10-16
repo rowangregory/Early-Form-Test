39eddf81701696119c865de932c6ba6ea2fbf3c3
rowangregory <rpgregory0404@jefcoed.com>
2025-10-16 09:19:23 -0500

    Made form.php in order to display data.
    I used the GET method.

---
+++ form.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Submitted Data</title>
+  </head>
+  <body>
+    Your day is <?php echo $_GET["prompt"] ?> today. What will it be like tomorrow>
+  </body>
+</html>
